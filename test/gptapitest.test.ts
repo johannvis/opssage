df78a640f6a24c50ce75775c77849462d9e4f25a
David Porter <57982995+davporte@users.noreply.github.com>
2025-10-13 12:58:02 +0800

    initial booting for cdk and typescript

---
+++ test/gptapitest.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Gptapitest from '../lib/gptapitest-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/gptapitest-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Gptapitest.GptapitestStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
